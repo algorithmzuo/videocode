0097f358bcddee998452ad5dd487ea12d30bfcb4
左程云 <algorithmzuo@163.com>
2020-12-21 14:42:26 +0800

    add new problems

---
+++ src/videocode/Code03_AppleMinBags.java
@@ -0,0 +1,48 @@
+package videocode;
+
+public class Code03_AppleMinBags {
+
+	public static int minBag(int apple) {
+		if (apple < 0) {
+			return -1;
+		}
+		if (apple == 0) {
+			return 0;
+		}
+		for (int max = (apple >> 3); max >= 0; max--) {
+			int rest = apple - (max << 3);
+			if (rest % 6 == 0) {
+				return max + rest / 6;
+			}
+		}
+		return -1;
+	}
+
+	public static int minBagAwesome(int apple) {
+		if (apple < 0 || (apple & 1) != 0) {
+			return -1;
+		}
+		if (apple == 0) {
+			return 0;
+		}
+		if (apple < 18) {
+			if (apple == 6 || apple == 8) {
+				return 1;
+			} else if (apple == 12 || apple == 14 || apple == 16) {
+				return 2;
+			} else {
+				return -1;
+			}
+		} else {
+			return (apple - 18) / 8 + 3;
+		}
+	}
+
+	public static void main(String[] args) {
+		for (int apple = 0; apple < 100; apple++) {
+			System.out.println(apple + " : " + minBag(apple));
+		}
+
+	}
+
+}
---
+++ src/videocode/Code04_EatGrass.java
@@ -0,0 +1,36 @@
+package videocode;
+
+public class Code04_EatGrass {
+
+	public static String winner1(int n) {
+		if (n < 5) {
+			return (n == 0 || n == 2) ? "后手" : "先手";
+		}
+		int base = 1;
+		while (base <= n) {
+			if (winner1(n - base).equals("后手")) {
+				return "先手";
+			}
+			if (base > n / 4) {
+				break;
+			}
+			base *= 4;
+		}
+		return "后手";
+	}
+
+	public static String winner2(int n) {
+		if (n % 5 == 0 || n % 5 == 2) {
+			return "后手";
+		} else {
+			return "先手";
+		}
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 50; i++) {
+			System.out.println(i + " : " + winner1(i));
+		}
+	}
+
+}
