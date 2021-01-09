cfe4eafbf4d40ceac0a76d4edf6b5e029a20ee4d
左程云 <algorithmzuo@163.com>
2021-01-09 12:49:25 +0800

    add new problems

---
+++ src/videocode/Code05_MaxSubArraySum.java
@@ -0,0 +1,81 @@
+package videocode;
+
+public class Code05_MaxSubArraySum {
+
+	public static int maxSubArraySum1(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return 0;
+		}
+		int N = arr.length;
+		int ans = Integer.MIN_VALUE;
+		for (int L = 0; L < N; L++) {
+			for (int R = L; R < N; R++) {
+				int sum = 0;
+				for (int i = L; i <= R; i++) {
+					sum += arr[i];
+				}
+				ans = Math.max(ans, sum);
+			}
+		}
+		return ans;
+	}
+
+	public static int maxSubArraySum2(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return 0;
+		}
+		int N = arr.length;
+		int[] dp = new int[N];
+		dp[0] = arr[0];
+		int ans = arr[0];
+		for (int i = 1; i < N; i++) {
+			dp[i] = Math.max(arr[i], arr[i] + dp[i - 1]);
+			ans = Math.max(ans, dp[i]);
+		}
+		return ans;
+	}
+
+	public static int maxSubArraySum3(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return 0;
+		}
+		int N = arr.length;
+		int pre = arr[0];
+		int ans = arr[0];
+		for (int i = 1; i < N; i++) {
+			pre = Math.max(arr[i], arr[i] + pre);
+			ans = Math.max(ans, pre);
+		}
+		return ans;
+	}
+
+	public static int[] randomArray(int maxLen, int maxValue) {
+		int len = (int) (Math.random() * (maxLen + 1));
+		int[] arr = new int[len];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) (Math.random() * (maxValue + 1)) - (int) (Math.random() * (maxValue + 1));
+		}
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		int maxLen = 20;
+		int maxValue = 50;
+		int testTime = 3000000;
+		System.out.println("如果没有错误信息打印，说明所有测试通过");
+		System.out.println("测试开始");
+		for (int i = 0; i < testTime; i++) {
+			int[] arr = randomArray(maxLen, maxValue);
+			int ans1 = maxSubArraySum1(arr);
+			int ans2 = maxSubArraySum2(arr);
+			int ans3 = maxSubArraySum3(arr);
+			if (ans1 != ans2 || ans1 != ans3) {
+				System.out.println("出错了！");
+				System.out.println(ans1 + " , " + ans2 + " , " + ans3);
+				break;
+			}
+		}
+		System.out.println("测试结束");
+	}
+
+}
---
+++ src/videocode/Code06_MaxSubMatrixSum.java
@@ -0,0 +1,151 @@
+package videocode;
+
+public class Code06_MaxSubMatrixSum {
+
+	public static int maxSubMatrixSum1(int[][] m) {
+		if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+			return 0;
+		}
+		int M = m.length;
+		int N = m[0].length;
+		int ans = Integer.MIN_VALUE;
+		for (int lur = 0; lur < M; lur++) {
+			for (int luc = 0; luc < N; luc++) {
+				for (int rdr = lur; rdr < M; rdr++) {
+					for (int rdc = luc; rdc < N; rdc++) {
+						int sum = 0;
+						for (int i = lur; i <= rdr; i++) {
+							for (int j = luc; j <= rdc; j++) {
+								sum += m[i][j];
+							}
+						}
+						ans = Math.max(ans, sum);
+					}
+				}
+			}
+		}
+		return ans;
+	}
+
+	public static int maxSubMatrixSum2(int[][] m) {
+		if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+			return 0;
+		}
+		int M = m.length;
+		int N = m[0].length;
+		int ans = Integer.MIN_VALUE;
+		for (int start = 0; start < M; start++) {
+			int[] arr = new int[N];
+			for (int cur = start; cur < M; cur++) {
+				for (int i = 0; i < N; i++) {
+					arr[i] += m[cur][i];
+				}
+				ans = Math.max(ans, maxSubArraySum(arr));
+			}
+		}
+		return ans;
+	}
+
+	public static int maxSubMatrixSum3(int[][] m) {
+		if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+			return 0;
+		}
+		m = m.length < m[0].length ? m : rotate(m);
+		int M = m.length;
+		int N = m[0].length;
+		int ans = Integer.MIN_VALUE;
+		for (int start = 0; start < M; start++) {
+			int[] arr = new int[N];
+			for (int cur = start; cur < M; cur++) {
+				for (int i = 0; i < N; i++) {
+					arr[i] += m[cur][i];
+				}
+				ans = Math.max(ans, maxSubArraySum(arr));
+			}
+		}
+		return ans;
+	}
+
+	public static int[][] rotate(int[][] m) {
+		int M = m.length;
+		int N = m[0].length;
+		int[][] ans = new int[N][M];
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				ans[j][i] = m[i][j];
+			}
+		}
+		return ans;
+	}
+
+	public static int maxSubArraySum(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return 0;
+		}
+		int N = arr.length;
+		int pre = arr[0];
+		int ans = arr[0];
+		for (int i = 1; i < N; i++) {
+			pre = Math.max(arr[i], arr[i] + pre);
+			ans = Math.max(ans, pre);
+		}
+		return ans;
+	}
+
+	public static int[][] randomMatrix(int maxLen, int maxValue) {
+		int rowSize = (int) (Math.random() * (maxLen + 1));
+		int colSize = (int) (Math.random() * (maxLen + 1));
+		int[][] m = new int[rowSize][colSize];
+		for (int i = 0; i < rowSize; i++) {
+			for (int j = 0; j < colSize; j++) {
+				m[i][j] = (int) (Math.random() * (maxValue + 1)) - (int) (Math.random() * (maxValue + 1));
+			}
+		}
+		return m;
+	}
+
+	public static void main(String[] args) {
+		int maxLen = 10;
+		int maxValue = 30;
+		int testTime = 30000;
+		System.out.println("功能测试开始");
+		System.out.println("如果没有错误信息打印，说明功能测试通过");
+		for (int i = 0; i < testTime; i++) {
+			int[][] m = randomMatrix(maxLen, maxValue);
+			int ans1 = maxSubMatrixSum1(m);
+			int ans2 = maxSubMatrixSum2(m);
+			int ans3 = maxSubMatrixSum3(m);
+			if (ans1 != ans2 || ans1 != ans3) {
+				System.out.println("出错了！");
+				System.out.println(ans1 + " , " + ans2 + " , " + ans3);
+				break;
+			}
+		}
+		System.out.println("功能测试结束");
+
+		System.out.println("性能测试开始");
+		int rowSize = 10000;
+		int colSize = 100;
+		int[][] m = new int[rowSize][colSize];
+		for (int i = 0; i < rowSize; i++) {
+			for (int j = 0; j < colSize; j++) {
+				m[i][j] = (int) (Math.random() * (maxValue + 1)) - (int) (Math.random() * (maxValue + 1));
+			}
+		}
+
+		long start;
+		long end;
+		start = System.currentTimeMillis();
+		int ans2 = maxSubMatrixSum2(m);
+		end = System.currentTimeMillis();
+		System.out.println("方法2的运行结果 : " + ans2 + " , 运行时间 : " + (end - start) + " ms");
+
+		start = System.currentTimeMillis();
+		int ans3 = maxSubMatrixSum3(m);
+		end = System.currentTimeMillis();
+		System.out.println("方法3的运行结果 : " + ans3 + " , 运行时间 : " + (end - start) + " ms");
+		System.out.println("性能测试结束");
+
+	}
+
+}
